2020b88e6750df93c7d414c48b874cab44aa7470
Shallker Wang <shallker.wang@profero.com>
2013-10-19 18:26:43 +0800

    First commit

---
+++ test/index.js
@@ -0,0 +1,22 @@
+var Http = require('../index');
+var test = require('simple-test');
+
+test('GET', function (done) {
+  var http = new Http;
+
+  http.GET('http://www.baidu.com', function (response) {
+    console.log('response');
+    done();
+  }, function (e) {
+    console.log('error', e);
+  });
+
+  http.on('buffer', function (buffer) {
+    console.log('buffer', buffer);
+  });
+
+  http.on('responseText', function (responseText) {
+    console.log('responseText', responseText);
+  });
+
+});
